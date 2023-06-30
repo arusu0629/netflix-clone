bdeb5f10c6dbffde8207fa4da81aa623a90c5550
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2023-06-30 16:32:52 +0900

    App.tsxでRowコンポーネントを配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { requests } from './request';
+import { Row } from './Row';
 
 function App() {
   return (
     <div className="App">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feachHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Document Movies" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
