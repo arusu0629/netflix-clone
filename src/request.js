4604cbac7c0e2b217e9f4c4be2dfc2dd843e8aa2
t-nakandakari <t-nakandakari@MacBook-Pro.local>
2023-07-01 18:29:33 +0900

    Action, Horrorはtvだとデータが取得できなかったのでmovieにリクエストURLを変更する

--- src/request.js
+++ src/request.js
@@ -5,9 +5,9 @@ export const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/discover/tv?api_key=${API_KEY}&languager=en-US`,
-    fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-    feachHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
+    feachHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
 }
