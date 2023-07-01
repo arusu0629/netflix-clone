987674296ed27f90e0bd693fbd2884449e5424a6
t-nakandakari <t-nakandakari@MacBook-Pro.local>
2023-07-01 15:57:21 +0900

    BannerのDOM部分を作成

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -36,4 +36,29 @@ export const Banner = () => {
       return str.length > n ? str?.substr(0, n - 1) + "..." : str;
     }
   }
+
+  return (
+    <header
+      className="Banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+       <div className="Banner-contents">
+        <h1 className="banner-title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="Banner-buttons">
+          <button className="Banner-button">Play</button>
+          <button className="Banner-button">My List</button>
+        </div>
+
+        <h1 className="Banner-description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+
+      <div className="Banner-fadeBottom" />
+    </header>
+  );
 };
